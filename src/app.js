b657f40632c69133289c5bc5940c43359ada4916
Dulen DP <ddp3093@gmail.com>
2025-02-28 20:36:15 +0530

    Enable CORS support for cross-origin requests and integrate payment routes into the application

--- src/app.js
+++ src/app.js
@@ -6,26 +6,14 @@ const connectDB = require("./config/db");
 dotenv.config(); // Load environment variables
 connectDB(); // Connect to the database
 
-
+const paymentRoutes = require("./routes/paymentRoutes");
 const app = express();
 
-// Middleware
-app.use(cors());
 app.use(express.json());
-
-const paymentRoutes = require("./routes/paymentRoutes");
-const authRoutes = require("./routes/authRoutes");
-const listingRoutes = require("./routes/listingRoutes");
-const userRoutes = require("./routes/userRoutes");
-
-
+app.use(cors());
 
 // Routes
-app.use("/api/auth", authRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/listings", listingRoutes);
 app.use("/api/payments", paymentRoutes);
-
 
 // Error handling middleware
 const { errorHandler } = require("./middlewares/errorHandler");
