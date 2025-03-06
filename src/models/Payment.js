2a70af3d0af2c3eb90414ba3d5d4006481f66e87
Dulen DP <ddp3093@gmail.com>
2025-03-06 16:04:48 +0530

    Changed the featureType stroing data to Premium and VIP and added the userRole to store in database

--- src/models/Payment.js
+++ src/models/Payment.js
@@ -4,7 +4,8 @@ const mongoose = require("mongoose");
 // Define the schema for storing payment details in MongoDB
 const paymentSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  featureType: { type: String, enum: ["stakeholder", "student"], required: true },
+  userRole: { type: String, enum: ["Landlord", "Student"], required: true }, //  Stores user role
+  featureType: { type: String, enum: ["Premium", "VIP"], required: true }, 
   amount: { type: Number, required: true },
   currency: { type: String, default: "LKR" },
   status: { type: String, enum: ["pending", "success", "failed"], default: "pending" },// Tracks payment status
