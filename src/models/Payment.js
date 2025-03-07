40a8fb78c3565afab4aba4de5cab5fa477054d9f
Dulen DP <ddp3093@gmail.com>
2025-03-07 15:13:26 +0530

    Updated Payment schema with subscription management.Renamed feature types to 'standard' and 'advanced'  .Added `boughtDate` field to track the purchase date .Introduced `subscriptionExpiry` to manage feature validity.Enabled automatic timestamps for better trackin

--- src/models/Payment.js
+++ src/models/Payment.js
@@ -5,12 +5,14 @@ const mongoose = require("mongoose");
 const paymentSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   userRole: { type: String, enum: ["Landlord", "Student"], required: true }, //  Stores user role
-  featureType: { type: String, enum: ["Premium", "VIP"], required: true }, 
+  featureType: { type: String, enum: ["standard", "advanced"], required: true }, 
   amount: { type: Number, required: true },
   currency: { type: String, default: "LKR" },
   status: { type: String, enum: ["pending", "success", "failed"], default: "pending" },// Tracks payment status
   transactionId: { type: String, unique: true,required: true},
-  createdAt: { type: Date, default: Date.now },// Timestamp for when the payment was made
-});
+  boughtDate:{ type: Date, default:Date.now },
+  subscriptionExpiry: { type: Date, required: true },
+  // Timestamp for when the payment was made
+} ,{ timestamps: true });
 
 module.exports = mongoose.model("Payments", paymentSchema);
