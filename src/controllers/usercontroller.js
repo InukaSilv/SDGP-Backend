64ae52b87e8f928f45fc8e0d3f32dc418a0b887c
Ishara Dharmapriya <isharadharmapriya145@gmail.com>
2025-03-18 21:25:48 +0530

    error fix

--- src/controllers/usercontroller.js
+++ src/controllers/usercontroller.js
@@ -1,5 +1,5 @@
 const User = require('../models/User');
-const { sendVerificationCode } = require("../utils/twilio");
+const { sendVerificationCode } = require("../utils/twillio");
 
 // @desc    Get user profile
 // @route   GET /api/users/profile
